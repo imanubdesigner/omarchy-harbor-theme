dc50e080f9f3b0c8a4e4f4f23b3b6f96571171be
HANCORE-linux <230438592+HANCORE-linux@users.noreply.github.com>
2025-12-17 20:45:16 +0100

    Add files via upload

---
+++ colors.fish
@@ -0,0 +1,27 @@
+set -U background '#dfe4c4'
+set -U foreground '#1c2d28'
+set -U cursor '#1c2d28'
+set -U color0 '#dfe4c4'
+set -U color1 '#bf616a'
+set -U color2 '#5f745d'
+set -U color3 '#d08770'
+set -U color4 '#5e81ac'
+set -U color5 '#b48ead'
+set -U color6 '#4a8c99'
+set -U color7 '#384f54'
+set -U color8 '#5f8170'
+set -U color9 '#bf616a'
+set -U color10 '#6b8c4f'
+set -U color11 '#d08770'
+set -U color12 '#5e81ac'
+set -U color13 '#b48ead'
+set -U color14 '#4a8c99'
+set -U color15 '#1c2d28'
+
+set -U fish_color_normal normal
+set -U fish_color_command green
+set -U fish_color_param cyan
+set -U fish_pager_color_completion
+set -U fish_pager_color_description $fish_color_quote yellow
+set -U fish_pager_color_progress brwhite --background=cyan
+set -U fish_color_history_current --bold
---
+++ neovim.lua
@@ -0,0 +1,44 @@
+return {
+    {
+        "bjarneo/aether.nvim",
+        name = "aether",
+        priority = 1000,
+        opts = {
+            disable_italics = false,
+            colors = {
+                -- Monotone shades (base00-base07)
+                base00 = "#dfe4c4", -- Default background
+                base01 = "#5f8170", -- Lighter background (status bars)
+                base02 = "#dfe4c4", -- Selection background
+                base03 = "#a07e8f", -- Comments, invisibles
+                base04 = "#384f54", -- Dark foreground
+                base05 = "#1c2d28", -- Default foreground
+                base06 = "#1c2d28", -- Light foreground
+                base07 = "#384f54", -- Light background
+
+                -- Accent colors (base08-base0F)
+                base08 = "#bf616a", -- Variables, errors, red
+                base09 = "#bf616a", -- Integers, constants, orange
+                base0A = "#d08770", -- Classes, types, yellow
+                base0B = "#556b2f", -- Strings, green
+                base0C = "#4a8c99", -- Support, regex, cyan
+                base0D = "#5e81ac", -- Functions, keywords, blue
+                base0E = "#4b7152", -- Keywords, storage, magenta
+                base0F = "#d08770", -- Deprecated, brown/yellow
+            },
+        },
+        config = function(_, opts)
+            require("aether").setup(opts)
+            vim.cmd.colorscheme("aether")
+            vim.o.background = "light"
+            -- Enable hot reload
+            require("aether.hotreload").setup()
+        end,
+    },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "aether",
+        },
+    },
+}
